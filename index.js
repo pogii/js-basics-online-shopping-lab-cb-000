f2e0651fadf650088316ef135e6e6060eac319dc
Wayne Thompson <wayne.thompsonjr@gmail.com>
2016-11-26 03:49:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -12,6 +12,36 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var p = Math.floor(Math.random() * 10)
+
+  var itemObject = {
+    name: item,
+    price: p
+  }
+  getCart().push(itemObject)
+  console.log(`${itemObject.name} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  var cart = getCart() // => [Object, Object] => e.g. { name: 'socks', price: 3 }
+  var cartLog = [] // =>
+
+  var message = 'In your cart, you have '
+  cartLog.unshift(message)
+
+  for(var i = 0, l = cart.length; i < l; i++) {
+    cartLog.push(`${getCart()[i].name} at $${getCart()[i].price}, `)
+  }
+
+  message = cartLog.join(' ')
+  console.log(message)
 }
